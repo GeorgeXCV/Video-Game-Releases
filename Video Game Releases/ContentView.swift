d711eb4e9593212ba42ba6893d18aa1a64c35d52
George <georgeashton95@gmail.com>
2019-10-06 23:19:55 +0100

    Initial Commit

---
+++ Video Game Releases/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Video Game Releases
+//
+//  Created by George on 06/10/2019.
+//  Copyright © 2019 George. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
